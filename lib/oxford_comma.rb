32b6fe60fe2f70856927d7ac2cbf6a7d0ac2ed48
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-01-31 19:36:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
+require "pry"
 def oxford_comma(array)
 if array.size ==1
   array.join
   elsif array.size ==2
   array.join(" and ")
 else array.size >=3
+  binding.pry
   array.insert(-2,"and")
   string = array.join(", ") 
   
