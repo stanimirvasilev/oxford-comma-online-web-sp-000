941fefe493977ff331f14823b085759354f967f2
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-01-31 19:27:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.size ==1
   array.join
   elsif array.size ==2
   array.join(" and ")
-elsif array.size >=3
+else array.size >=3
   string = array.join(", ") 
   string.insert(-1 , "and")
  
